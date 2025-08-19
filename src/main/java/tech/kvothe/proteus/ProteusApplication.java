cf0565db8280603c15d36d6a7c4fdd61b8f5d3a1
Ítalo H. <italodssouza.backup@gmail.com>
2025-08-19 15:12:06 -0300

    First Commit

---
+++ src/main/java/tech/kvothe/proteus/ProteusApplication.java
@@ -0,0 +1,13 @@
+package tech.kvothe.proteus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProteusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProteusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/tech/kvothe/proteus/ProteusApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.kvothe.proteus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProteusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
